7ce4a0ad2a3e3ca722b246236b1fd091d3dd443b
Timerbulat <timerhelmut@gmail.com>
2023-09-26 22:04:29 +0300

    upload tasks 6-11

--- Task4.cpp
+++ Task4.cpp
@@ -1,46 +1,46 @@
-#include <iostream>
-#include <omp.h>
-
-int main() {
-    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int b[10] = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11};
-
-    int minA = a[0];
-    int maxB = b[0];
-
-    const int num_threads = 2;
-#pragma omp parallel num_threads(num_threads)
-    {
-        int threadId = omp_get_thread_num();
-
-        if (threadId == 0) {
-            for (int i = 1; i < 10; ++i) {
-                if (a[i] < minA) {
-#pragma omp critical
-                    {
-                        if (a[i] < minA) {
-                            minA = a[i];
-                        }
-                    }
-                }
-            }
-        }
-        if (threadId == 1) {
-            for (int i = 1; i < 10; ++i) {
-                if (b[i] > maxB) {
-#pragma omp critical
-                    {
-                        if (b[i] > maxB) {
-                            maxB = b[i];
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    printf("min from a: %d\n", minA);
-    printf("max from b: %d\n", maxB);
-
-    return 0;
-}
+//#include <iostream>
+//#include <omp.h>
+//
+//int main() {
+//    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//    int b[10] = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11};
+//
+//    int minA = a[0];
+//    int maxB = b[0];
+//
+//    const int num_threads = 2;
+//#pragma omp parallel num_threads(num_threads)
+//    {
+//        int threadId = omp_get_thread_num();
+//
+//        if (threadId == 0) {
+//            for (int i = 1; i < 10; ++i) {
+//                if (a[i] < minA) {
+//#pragma omp critical
+//                    {
+//                        if (a[i] < minA) {
+//                            minA = a[i];
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        if (threadId == 1) {
+//            for (int i = 1; i < 10; ++i) {
+//                if (b[i] > maxB) {
+//#pragma omp critical
+//                    {
+//                        if (b[i] > maxB) {
+//                            maxB = b[i];
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    printf("min from a: %d\n", minA);
+//    printf("max from b: %d\n", maxB);
+//
+//    return 0;
+//}
--- Task5.cpp
+++ Task5.cpp
@@ -1,83 +1,83 @@
-#include <iostream>
-#include <omp.h>
-#include <cstdlib>
-#include <ctime>
-
-int main() {
-    const int rows = 6;
-    const int cols = 8;
-    int d[rows][cols];
-
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            d[i][j] = std::rand() % 100;
-        }
-    }
-
-    double avg = 0;
-    int minArr = d[0][0];
-    int maxArr = d[0][0];
-    int countBy3 = 0;
-
-#pragma omp parallel sections num_threads(3)
-    {
-#pragma omp section
-        {
-            int threadId = omp_get_thread_num();
-            int localSum = 0;
-
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < cols; ++j) {
-                    localSum += d[i][j];
-                }
-            }
-
-#pragma omp critical
-            {
-                avg = static_cast<double>(localSum) / (rows * cols);
-                std::cout << "Section 1, thread id: " << threadId << ": average = " << avg << std::endl;
-            }
-        }
-
-#pragma omp section
-        {
-            int threadId = omp_get_thread_num();
-
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < cols; ++j) {
-                    if (d[i][j] < minArr) {
-                        minArr = d[i][j];
-                    }
-                    if (d[i][j] > maxArr) {
-                        maxArr = d[i][j];
-                    }
-                }
-            }
-#pragma omp critical
-
-            printf("Section 2, thread id: %d, min=%d, max=%d\n", threadId, minArr, maxArr);
-        }
-
-#pragma omp section
-        {
-            int threadId = omp_get_thread_num();
-
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < cols; ++j) {
-                    if (d[i][j] % 3 == 0) {
-#pragma omp atomic
-                        countBy3++;
-                    }
-                }
-            }
-#pragma omp critical
-
-            printf("Section 3, thread id: %d, count=%d\n", threadId, countBy3);
-        }
-    }
-
-    return 0;
-}
-
-
+//#include <iostream>
+//#include <omp.h>
+//#include <cstdlib>
+//#include <ctime>
+//
+//int main() {
+//    const int rows = 6;
+//    const int cols = 8;
+//    int d[rows][cols];
+//
+//    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+//    for (int i = 0; i < rows; ++i) {
+//        for (int j = 0; j < cols; ++j) {
+//            d[i][j] = std::rand() % 100;
+//        }
+//    }
+//
+//    double avg = 0;
+//    int minArr = d[0][0];
+//    int maxArr = d[0][0];
+//    int countBy3 = 0;
+//
+//#pragma omp parallel sections num_threads(3)
+//    {
+//#pragma omp section
+//        {
+//            int threadId = omp_get_thread_num();
+//            int localSum = 0;
+//
+//            for (int i = 0; i < rows; ++i) {
+//                for (int j = 0; j < cols; ++j) {
+//                    localSum += d[i][j];
+//                }
+//            }
+//
+//#pragma omp critical
+//            {
+//                avg = static_cast<double>(localSum) / (rows * cols);
+//                std::cout << "Section 1, thread id: " << threadId << ": average = " << avg << std::endl;
+//            }
+//        }
+//
+//#pragma omp section
+//        {
+//            int threadId = omp_get_thread_num();
+//
+//            for (int i = 0; i < rows; ++i) {
+//                for (int j = 0; j < cols; ++j) {
+//                    if (d[i][j] < minArr) {
+//                        minArr = d[i][j];
+//                    }
+//                    if (d[i][j] > maxArr) {
+//                        maxArr = d[i][j];
+//                    }
+//                }
+//            }
+//#pragma omp critical
+//
+//            printf("Section 2, thread id: %d, min=%d, max=%d\n", threadId, minArr, maxArr);
+//        }
+//
+//#pragma omp section
+//        {
+//            int threadId = omp_get_thread_num();
+//
+//            for (int i = 0; i < rows; ++i) {
+//                for (int j = 0; j < cols; ++j) {
+//                    if (d[i][j] % 3 == 0) {
+//#pragma omp atomic
+//                        countBy3++;
+//                    }
+//                }
+//            }
+//#pragma omp critical
+//
+//            printf("Section 3, thread id: %d, count=%d\n", threadId, countBy3);
+//        }
+//    }
+//
+//    return 0;
+//}
+//
+//
